05bf43e278b43b3b324901968b4ddcec2a1e3dff
Ana Vargas <61504223+macedo19@users.noreply.github.com>
2022-10-01 23:54:08 -0300

    Primeiro Commit

---
+++ android/app/src/main/kotlin/com/example/devops/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.devops
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/AppInicial.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class AppInicial extends StatefulWidget {
+
+  @override
+  State<AppInicial> createState() => _AppInicialState();
+}
+
+class _AppInicialState extends State<AppInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Teste de DEVOPS"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}

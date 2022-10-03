eb83b22b49dcd28af6c59658d6eea9a5226c8b81
Lucas Damião <lucasdamioamartni@hotmail.com>
2022-10-02 22:53:15 -0300

    Models

---
+++ lib/Model/Saldo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Saldo {
+  String timeCasa;
+  String timeVisitante;
+  double saldo;
+  dynamic dateJogo;
+  String escudoCasa;
+  String escudoVisitante;
+  String timeEscolhido;
+
+
+  Saldo({
+    required this.timeCasa,
+    required this.timeVisitante,
+    required this.saldo,
+    required this.dateJogo,
+    required this.escudoCasa,
+    required this.escudoVisitante,
+    required this.timeEscolhido,
+
+  });
+}
